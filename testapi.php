b3cb743cb46ef10df37ce569f09a194a5fbe4edc
yeftakun <yeftakun34@gmail.com>
2024-06-10 08:45:40 +0800

    update test api

--- testapi.php
+++ testapi.php
@@ -1,5 +1,5 @@
 <?php
-$telegramAPI = "https://api.telegram.org/bot6906279097:AAHN_mpUN7GqUX_jqT8upKdjzsbQmNB3b-U/sendMessage?parse_mode=markdown&chat_id=1627790263&text=Hello%20hehe";
+$telegramAPI = "https://api.telegram.org/bot6906279097:AAHN_mpUN7GqUX_jqT8upKdjzsbQmNB3b-U/sendMessage?parse_mode=markdown&chat_id=1627790263&text=Cron%20Job%20is%20running%20every%20minute.";
 $ch = curl_init();
 
 // Set opsi cURL
@@ -10,6 +10,4 @@ curl_exec($ch);
 
 // Tutup cURL
 curl_close($ch);
-
-
 ?>
