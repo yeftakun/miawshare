da73ff3c9ca7b8de8a17f2f7bbeb98e368e2c847
yeftakun <yeftakun34@gmail.com>
2024-07-23 22:38:13 +0800

    add var

--- template_environment.php
+++ template_environment.php
@@ -1,7 +1,8 @@
 <?php
 // Definisikan variabel token bot
 define('TOKEN_BOT', 'ISI TOKEN BOT TELEGRAM DISINI');
-define('MAX_IMAGE_SIZE', 512000);
+define('MAX_IMAGE_SIZE', 1024000);
+define('MAX_PROFILE_IMAGE_SIZE', 1024000);
 define('OWNER_CHAT_ID', 'YOUR_CHAT_ID');
 define('LIMIT_BERANDA', 100);
 ?>
