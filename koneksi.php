630df71e04d2f04a11969b1187726bae3a27704f
yeftakun <yeftakun34@gmail.com>
2024-07-27 20:35:58 +0800

    update connection detail

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,6 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","db_community");
+// $koneksi = mysqli_connect("osl.h.filess.io","first_tobaccobat","hehehe123","first_tobaccobat","3307");
+$koneksi = mysqli_connect("localhost","root","","db_community","3306");
  
 // Check connection
 if (mysqli_connect_errno()){
