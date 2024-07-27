20cb5f07ce4a688aa475a5818b26da7ee7647c42
yeftakun <yeftakun34@gmail.com>
2024-07-27 20:49:39 +0800

    Update terms of service page content

--- pages/terms.php
+++ pages/terms.php
@@ -13,7 +13,8 @@
         <h1>MiawShare - Ketentuan Layanan</h1>
 
         <h2>Layanan Gratis</h2>
-        <p>MiawShare menyediakan layanan unggah gambar tanpa biaya untuk semua pengguna. Pendaftaran diperlukan dengan menggunakan ChatID Telegram agar pengguna dapat mengunggah gambar. Namun, untuk melihat dan menggunakan gambar, pendaftaran tidak diperlukan (akses tamu). Dengan memanfaatkan layanan ini, Anda menyetujui semua ketentuan penggunaan yang berlaku.</p>
+        <!-- <p>MiawShare menyediakan layanan unggah gambar tanpa biaya untuk semua pengguna. Pendaftaran diperlukan dengan menggunakan ChatID Telegram agar pengguna dapat mengunggah gambar. Namun, untuk melihat dan menggunakan gambar, pendaftaran tidak diperlukan (akses tamu). Dengan memanfaatkan layanan ini, Anda menyetujui semua ketentuan penggunaan yang berlaku.</p> -->
+        <p>MiawShare menyediakan layanan unggah gambar tanpa biaya untuk semua pengguna. Pengguna dapat melihat dan menggunakan gambar tanpa mendaftar (akses tamu). Namun untuk mengunggah gambar pengguna perlu mendaftar terlebih dahulu dan akun telegram (ChatID) diperlukan sebagai tujuan pengiriman notifikasi dan otp kepada pengguna.</p>
 
         <h2>Konten yang Layak</h2>
         <p>Kami menghargai kebebasan berekspresi dan kreativitas, tetapi juga mengutamakan keamanan dan kenyamanan semua pengguna. Pastikan untuk hanya mengunggah gambar yang sesuai dan tidak mengandung konten negatif seperti:</p>
@@ -24,13 +25,17 @@
             <li>Informasi pribadi tanpa izin</li>
             <li>Konten yang ilegal atau berbahaya</li>
         </ul>
-        <p>Kami memiliki hak untuk menghapus gambar yang dianggap tidak sesuai tanpa pemberitahuan terlebih dahulu.</p>
+        <p>Pengguna dapat melaporkan gambar yang dianggap melanggar ketentuan layanan kami di fitur laporkan. Kami memiliki hak untuk menghapus gambar yang dianggap tidak sesuai tanpa pemberitahuan terlebih dahulu.</p>
 
         <h2>Batasan Tanggung Jawab</h2>
-        <p>Kami tidak bertanggung jawab atas konten yang diunggah oleh pengguna. Semua tanggung jawab berada pada pengguna yang mengunggah gambar. Jika Anda menemukan gambar yang melanggar hak cipta atau tidak pantas, harap gunakan fitur pelaporan gambar. Pada saat pendaftaran, pengguna diminta untuk memasukkan ChatID Telegram yang akan digunakan untuk mengirim pesan melalui bot Telegram. Jika bot kami mengirimkan pesan spam, kami tidak bertanggung jawab dan Anda dapat memblokir bot tersebut. ChatID Telegram hanya bisa digunakan jika pengguna telah mengirim pesan kepada bot, sehingga bot dapat mengenali ChatID tersebut.</p>
+        <p>Kami tidak bertanggung jawab atas konten yang diunggah oleh pengguna. Semua tanggung jawab berada pada pengguna yang mengunggah gambar. Jika Anda menemukan gambar yang melanggar hak cipta atau tidak pantas, harap gunakan fitur laporkan.</p>
+        <p>Pada saat pendaftaran, pengguna diminta untuk memasukkan ChatID Telegram yang akan digunakan untuk mengirim pesan melalui bot Telegram. Jika bot kami mengirimkan pesan spam pengguna dapat segera memblokir bot tersebut dan pengguna dapat melaporkannya di fitur setting > tekan tombol "Laporkan Bot Spam", kami tidak bertanggung jawab dan Anda dapat memblokir bot tersebut. ChatID Telegram hanya bisa digunakan jika pengguna telah mengirim pesan kepada bot, sehingga bot dapat mengenali ChatID tersebut.</p>
 
         <h2>Batasan dan Penyalahgunaan</h2>
-        <p>Pengunggahan konten yang ilegal, berbahaya, atau yang melanggar hak cipta adalah dilarang. Kami berhak menghapus konten yang melanggar ketentuan tanpa pemberitahuan dan membatasi akses pengguna jika diperlukan. Penggunaan yang berulang kali melanggar ketentuan dapat mengakibatkan pemblokiran IP. Pengguna juga dilarang untuk menggunakan kata-kata tidak pantas dalam informasi akun seperti nama, username, dan bio.</p>
+        <p>Pengunggahan konten yang ilegal, berbahaya, atau yang melanggar hak cipta adalah dilarang. Kami berhak menghapus konten yang melanggar ketentuan tanpa pemberitahuan dan membatasi akses pengguna jika diperlukan.</p>
+        <p>Pengguna juga dilarang untuk menggunakan kata-kata tidak pantas dalam informasi akun seperti nama, username, dan bio.</p>
+        <p>Pengguna dilarang melakukan spam tanpa alasan pada pelaporan spam bot, pelaporan postingan, maupun kontak kami.</p>
+        <p>Penggunaan yang berulang kali melanggar ketentuan dapat mengakibatkan pemblokiran IP dan pemblokiran akun pengguna.</p>
 
         <p>Harap selalu periksa kebijakan penggunaan kami secara rutin.</p>
     </div>
