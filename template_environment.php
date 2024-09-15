8e2a91bc66678ae37b55a1849b25478d31b30c85
yeftakun <yeftakun34@gmail.com>
2024-09-15 20:19:25 +0800

    NSFW detection option

--- template_environment.php
+++ template_environment.php
@@ -1,8 +1,8 @@
 <?php
-// Definisikan variabel token bot
 define('TOKEN_BOT', 'ISI TOKEN BOT TELEGRAM DISINI');
 define('MAX_IMAGE_SIZE', 1024000);
 define('MAX_PROFILE_IMAGE_SIZE', 1024000);
 define('OWNER_CHAT_ID', 'YOUR_CHAT_ID');
 define('LIMIT_BERANDA', 100);
+define('NSFW_DETECT', 1) // 1 for on, 0 for off
 ?>
