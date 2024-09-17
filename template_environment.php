762f4227cb6194b7e2c4b53ba8a9b08a723c2771
yeftakun <yeftakun34@gmail.com>
2024-09-18 01:20:55 +0800

    host address

--- template_environment.php
+++ template_environment.php
@@ -4,5 +4,7 @@ define('MAX_IMAGE_SIZE', 1024000);
 define('MAX_PROFILE_IMAGE_SIZE', 1024000);
 define('OWNER_CHAT_ID', 'YOUR_CHAT_ID');
 define('LIMIT_BERANDA', 100);
-define('NSFW_DETECT', 1) // 1 for on, 0 for off
+define('NSFW_DETECT', 1) // 1 for on
+define('HOST_MAIN', 'http://localhost/miawshare');
+define('HOST_API', 'http://localhost:5000');
 ?>
